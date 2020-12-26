0cfaa8c4123aeec63461551230968c180a59b55f
softmarshmallow <woojoo@softmarshmallow.com>
2020-12-26 18:38:48 +0900

    added color scheme and gradients seed for buttons

---
+++ lib/seeds/color-schemes/button.color-scheme.seed.ts
@@ -0,0 +1,70 @@
+import { ButtonColorScheme } from "@reflect.bridged.xyz/core/lib/theme/color-schemes"
+
+export const BUTTON_COLOR_SCHEMES_SET: Array<ButtonColorScheme> = [
+    {
+        base: '#151818',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#5B7FFF',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#FF835B',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#E93843',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#FFE175',
+        text: '#000000'
+    },
+    //
+
+    {
+        base: '#4DD091',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#4AB868',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#FFFFFF',
+        text: '#4F60FF',
+        border: '#4F60FF'
+    },
+    {
+        base: '#F15223',
+        text: '#FFFFFF',
+    },
+    {
+        base: '#06C1FF',
+        text: '#FFFFFF'
+    },
+
+    //
+    {
+        base: '#8A24FF',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#5041AB',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#4A5CB8',
+        text: '#FFFFFF'
+    },
+    {
+        base: '#FFFFFF',
+        text: '#E15869',
+        border: '#E15869'
+    },
+    {
+        base: '#71C297',
+        text: '#FFFFFF'
+    },
+]
